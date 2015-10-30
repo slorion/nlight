8f530f1b8f507bfe1611a52e7acd55015fa94998
slorion <github@thestrangefactory.com>
2015-10-30 11:32:35 -0400

    Added collection extensions

---
+++ src/NLight/Collections/IListExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Author(s): Sébastien Lorion
+
+using NLight.Core;
+using System;
+using System.Collections.Generic;
+
+namespace NLight.Collections
+{
+	public static class IListExtensions
+	{
+		public static void Shuffle<T>(this IList<T> items, int iterationCount = 1000)
+		{
+			if (items == null) throw new ArgumentNullException(nameof(items));
+			if (iterationCount < 0) throw new ArgumentOutOfRangeException(nameof(iterationCount), iterationCount, null);
+
+			for (int i = 0; i < iterationCount; i++)
+			{
+				int from = StrongRandom.NextInt32(0, items.Count);
+				int to = StrongRandom.NextInt32(0, items.Count);
+
+				var temp = items[from];
+				items[from] = items[to];
+				items[to] = temp;
+			}
+		}
+	}
+}
