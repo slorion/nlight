59cb94cd7b32b4b389f5d3302bcc0458637b16a2
slorion <github@thestrangefactory.com>
2015-10-30 11:31:00 -0400

    Added core utilities and extensions

---
+++ src/NLight/Core/PreciseDateTime.cs
@@ -0,0 +1,37 @@
+﻿// Author(s): Sébastien Lorion
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace NLight.Core
+{
+	public static class PreciseDateTime
+	{
+		// kernel function is available in Windows 8/2012+
+		// https://msdn.microsoft.com/en-us/library/windows/desktop/hh706895%28v=vs.85%29.aspx
+		[DllImport("Kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+		private static extern void GetSystemTimePreciseAsFileTime(out long filetime);
+
+		private static readonly Func<DateTimeOffset> _getNow;
+
+		static PreciseDateTime()
+		{
+			try
+			{
+				_getNow =
+					() =>
+					{
+						long filetime;
+						GetSystemTimePreciseAsFileTime(out filetime);
+						return DateTimeOffset.FromFileTime(filetime);
+					};
+			}
+			catch (EntryPointNotFoundException)
+			{
+				_getNow = () => DateTimeOffset.Now;
+			}
+		}
+
+		public static DateTimeOffset Now => _getNow();
+	}
+}
---
+++ src/NLight/Core/StrongRandom.cs
@@ -0,0 +1,55 @@
+﻿// Author(s): Sébastien Lorion
+
+using System;
+using System.Security.Cryptography;
+
+namespace NLight.Core
+{
+	// could be made faster by buffering calls to GetBytes, but thread-safety issues would need to be dealt with
+	public static class StrongRandom
+	{
+		private static readonly RandomNumberGenerator _rng = RandomNumberGenerator.Create();
+
+		public static int NextInt32(int max) => NextInt32(0, max);
+
+		public static int NextInt32(int min, int max)
+		{
+			if (max < min) throw new ArgumentOutOfRangeException(nameof(max), max, null);
+
+			int value = BitConverter.ToInt32(NextBytes(sizeof(int)), 0) & 0x7FFF;
+			int range = max - min;
+
+			return (value % range) + min;
+		}
+
+		public static long NextInt64(long max) => NextInt64(0, max);
+
+		public static long NextInt64(long min, long max)
+		{
+			if (max < min) throw new ArgumentOutOfRangeException(nameof(max), max, null);
+
+			long value = BitConverter.ToInt64(NextBytes(sizeof(long)), 0) & 0x7FFFFFFF;
+			long range = max - min;
+
+			return (value % range) + min;
+		}
+
+		public static byte[] NextBytes(int count)
+		{
+			if (count < 0) throw new ArgumentOutOfRangeException(nameof(count), count, null);
+
+			var bytes = new byte[count];
+			_rng.GetBytes(bytes);
+
+			return bytes;
+		}
+
+		public static void NextBytes(byte[] buffer, int offset, int count)
+		{
+			if (offset < 0) throw new ArgumentOutOfRangeException(nameof(offset), offset, null);
+			if (count < 0 || offset + count > buffer.Length) throw new ArgumentOutOfRangeException(nameof(count), count, null);
+
+			_rng.GetBytes(buffer, offset, count);
+		}
+	}
+}
