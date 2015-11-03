fe1b8ef624307befb0d6e1c3b8ce0d9b36bd3c34
slorion <github@thestrangefactory.com>
2015-11-03 17:57:24 -0500

    Updated nuspec and added push script

---
+++ src/nuget-push.ps1
@@ -0,0 +1,15 @@
+﻿$project_name = "NLight"
+
+cd $project_name
+
+del *.nupkg -Force
+
+..\.nuget\nuget.exe pack ($project_name + ".csproj") -Properties Configuration=Release -Build -IncludeReferencedProjects -NonInteractive
+
+$pkg = dir *.nupkg | Select-Object -Last 1
+
+Write-Host $pkg
+
+..\.nuget\nuget.exe push $pkg
+
+pause
