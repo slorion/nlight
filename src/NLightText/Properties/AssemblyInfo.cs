08641dc934da1f0e70cfde252a185a6100069d45
Atif Aziz <code@raboof.com>
2016-02-25 08:19:46 +0100

    Separate project with only NLight.IO.Text & supporting bits
    Adds to issue #1

---
+++ src/NLightText/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NLightText")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("1aedc7d9-b983-4e4d-8f24-cc4b82c0db48")]
