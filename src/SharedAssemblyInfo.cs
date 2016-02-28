5c69119641cb99f378fc46e086885445cdea48e5
slorion <github@thestrangefactory.com>
2016-02-27 23:23:17 -0500

    Updated version to 1.0.2

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
